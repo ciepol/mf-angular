c93be33de6d1bc4b5d7eb05bd6ae840d86dd61cd
ciepol <ciepol@wp.pl>
2014-05-08 17:41:44 +0200

    modRewrite

--- web.js
+++ web.js
@@ -1,8 +1,11 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var morgan  = require('morgan');
+var modRewrite = require('connect-modrewrite');
 var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.use(modRewrite(['!(\\..+)$ / [L]']));
+app.use(connect.static(options.base));
 app.listen(process.env.PORT || 5000);
