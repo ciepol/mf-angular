92156841aff013ce659829febd91f6c2401d9166
ciepol <ciepol@wp.pl>
2014-05-08 15:31:18 +0200

    build fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -281,6 +281,7 @@ module.exports = function (grunt) {
             '*.{ico,png,txt}',
             '.htaccess',
             '*.html',
+            'data/{,*/}*.json',
             'views/{,*/}*.html',
             'images/{,*/}*.{webp}',
             'fonts/*'
